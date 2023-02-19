bcf9d664c1e14f6e3e9594ac6232aa7b1bbfaf41
3782291211 <Laklouk98@hotmail.com>
2023-02-19 20:25:29 +0000

    Adding project list

---
+++ src/components/projectsList.js
@@ -0,0 +1,88 @@
+export const projects = [
+    {
+        "image": "three-js.png",
+        "project name": "3D React app",
+        "type": "Front end",
+        "tech": `
+React, Three.js, Bootstrap`,
+        "description": `
+A 3D react app created using
+Three.js, featuring a 3D and 2D
+view based on recursive geometric
+computations.`,
+"link": "https://3782291211.github.io/3D-geometric-sim/",
+"github": "https://github.com/3782291211/3D-geometric-sim"
+    },
+    {
+        "image": "logo512.png",
+        "project name": "React social app",
+        "type": "Front end",
+        "tech": `
+React`,
+        "description": `
+A responsive React web app centred
+around data fetching and boasting
+full CRUD functionality,
+comprehensive error handling and a
+design process explicitly focused
+on UI optimisations for mobile.`,
+"link": "https://nc-news-react-app.netlify.app/",
+"github": "https://github.com/3782291211/frontend-portfolio-project"
+    },
+    {
+        "image": "node-express.png",
+        "project name": "Node/Express REST API",
+        "type": "Back end",
+        "tech": `
+Node, Express`,
+        "description": `
+A comprehensive REST API featuring
+full CRUD functionality, exhaustive
+integration testing, advanced error
+handling, relational data models,
+complex SQL join queries, pagination,
+modular routing, and more.`,
+"link": "https://majids-backend-api-project.onrender.com/"
+    },
+    {
+        "image": "vue.svg",
+        "project name": "Vue app",
+        "type": "Front end",
+        "tech": `
+Vue, Bootstrap`,
+        "description": `
+A Vue.js app that provides an
+interface for one of my API
+projects.`,
+"link": "https://3782291211.github.io/vue-api-interface/",
+"github": "https://github.com/3782291211/backend-portfolio-project"
+    },
+    {
+        "image": "drf.png",
+        "project name": "NoSQL Python/Django API",
+        "type": "Back end",
+        "tech": `
+Django, MongoDB`,
+        "description": `
+A Django REST Framework API
+supporting all CRUD operations,
+complete with comprehensive error
+handling and full-coverage
+integration testing.`,
+"link": "https://automatrixapi.pythonanywhere.com/",
+"github": "https://github.com/3782291211/django-api-project"
+    },
+    {
+        "image": "html.png",
+        "project name": "Pop culture website",
+        "type": "Front end",
+        "tech": `
+HTML, CSS, JavaScript`,
+        "description": `
+An HTML/CSS project focused on
+responsive web design principles
+and attractive styling.`,
+"link": "https://3782291211.github.io/HTML-CSS-portfolio-project/",
+"github": "https://github.com/3782291211/HTML-CSS-portfolio-project"
+    }
+];
