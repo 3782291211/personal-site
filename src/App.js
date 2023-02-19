24ad400f0426ab698112757023a1c92e07a223bf
3782291211 <Laklouk98@hotmail.com>
2023-02-19 20:24:04 +0000

    Adding canvas component to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Canvas } from '@react-three/fiber';
 import Home from './components/Home';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
+      <Canvas camera={{
+        fov: 70
+      }} className='home_canvas'>
       <Home/>
+      </Canvas>
     </div>
   );
 }
