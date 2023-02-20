02e51a11cd32886e6c63ef949f2b910a36eae510
3782291211 <Laklouk98@hotmail.com>
2023-02-20 04:01:39 +0000

    Adding navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export function Navigation() {
+    return(<nav>
+        <Link to="/">Home</Link>
+        <Link to="projects">Projects</Link>
+        <Link target='#' to="https://github.com/3782291211">Github</Link>
+        <Link to="about">About</Link>
+    </nav>)
+};
