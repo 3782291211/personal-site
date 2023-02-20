3503c5b8050de112d6eadfa570cff35656a78857
3782291211 <Laklouk98@hotmail.com>
2023-02-20 04:04:43 +0000

    Adding rotating sun with godrays

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,40 +1,61 @@
-import React, { Suspense, useRef, useState } from "react";
-import { OrbitControls, SpotLight, Stars, useDepthBuffer } from "@react-three/drei";
-import "../index.css";
-import { Projects, Title } from "./Text";
-import { Loading } from "./Loading";
-import { Bloom, EffectComposer, Glitch, Noise, Scanline, Vignette } from "@react-three/postprocessing";
-import { BlendFunction, GlitchMode } from 'postprocessing'
-import { useThree } from "@react-three/fiber";
+import { useFrame } from "@react-three/fiber";
+import { EffectComposer, GodRays } from "@react-three/postprocessing";
+import { forwardRef } from "react";
+import { BlendFunction, KernelSize, Resizer } from "postprocessing";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { useResource } from "react-three-fiber";
 
 
-export default function Home() {
-  const [glitch, setGlitch] = useState(true);
-  const { viewport } = useThree();
-  const meshRef = useRef();
-  console.log(viewport);
-
-  return (<mesh ref={meshRef} scale={viewport.width < 5.5 ? viewport.width / 6 : 1}>
-      <Stars/>
-      <directionalLight intensity={0.5} />
-      <SpotLight anglePower={8} position={[3, 3, 5]}/>
-      <SpotLight anglePower={8} position={[-3, 3, 5]}/>
-      <OrbitControls></OrbitControls>
-      <EffectComposer>
-        <Bloom 
-        mipmapBlur 
-        luminanceThreshold={1}
-        intensity={3}
-        />
-        <Scanline blendFunction={BlendFunction.DARKEN} density={5} opacity={0.1}/>
-        <Noise opacity={0.1}/>
-        {glitch && <Glitch mode={GlitchMode.SPORADIC} strength={[0.2, 0.7]} delay={[1.5, 6]} ratio={0.5} duration={[0.05, 0.6]} />}
-        <Vignette  offset={0.7} darkness={0.55} blendFunction={BlendFunction.NORMAL}/>
-      <Suspense fallback={<Loading/>}>
-      <Title/>
-      <Projects glitch={glitch} setGlitch={setGlitch}/>
-      </Suspense>
-      </EffectComposer>
+const Sun = forwardRef(function Sun(props, forwardRef) {
+    useFrame(({ clock }) => {
+      forwardRef.current.position.x = Math.sin(clock.getElapsedTime()) * -9;
+      forwardRef.current.position.y = Math.cos(clock.getElapsedTime()) * -9;
+    });
+  
+    return (
+      <mesh ref={forwardRef} position={[0, 0, -15]}>
+        <sphereGeometry args={[1, 36, 36]} />
+        <meshBasicMaterial color={"#3564FF"} />
       </mesh>
+    );
+  });
+  
+function Effects() {
+  const sunRef = useResource(null);
+  return (
+    <>
+      <Sun ref={sunRef} />
+      {sunRef.current && (
+        <EffectComposer multisampling={0}>
+          <GodRays
+            sun={sunRef.current}
+            blendFunction={BlendFunction.Screen}
+            samples={30}
+            density={0.97}
+            decay={0.96}
+            weight={0.6}
+            exposure={0.4}
+            clampMax={1}
+            width={Resizer.AUTO_SIZE}
+            height={Resizer.AUTO_SIZE}
+            kernelSize={KernelSize.SMALL}
+            blur={true}
+          />
+        </EffectComposer>
+      )}
+    </>
   );
-}
+};
+
+
+export function Home () {
+    return (
+        <mesh>
+        <PerspectiveCamera makeDefault fov={65} position={[0, 0, 40]} />
+        <Effects/>
+        <OrbitControls/>
+        <ambientLight intensity={0.8} />
+        <pointLight position={[15, 15, 15]} intensity={1} />
+        </mesh>
+        );
+    };
